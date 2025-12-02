928c171c1f95d8c254457bcac061880d389304cc
mahegiogetiluciani-stack <mahegiogetiluciani@gmail.com>
2025-12-02 21:27:46 +0100

    Add files via upload

---
+++ mark.py
@@ -0,0 +1,9 @@
+print("quelle est la marque de vo chosure")
+marque_chaussure = input()
+if marque_chaussure == "nike":
+    print("accepter")
+else:
+    if marque_chaussure == "jiselle":
+        print("chaussure refuser")
+    else:
+        print("refuser")
