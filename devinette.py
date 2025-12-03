dc2e44f8c41de091bec8ed22cbe38300c3c72c2c
mahegiogetiluciani-stack <mahegiogetiluciani@gmail.com>
2025-12-03 19:45:53 +0100

    Add files via upload

---
+++ devinette.py
@@ -0,0 +1,20 @@
+print("b__bon")
+e = input("Entre le mot complet : ")
+a_e = "bonbon"
+
+if e == a_e:
+    print("correct")
+else:
+    print("faux")
+
+print("b___our")
+a = input("Entre le mot complet : ")
+e_a = "bonjour"
+
+if a == e_a:
+    print("correct")
+else:
+    print("faux")
+
+print("fin")
+
