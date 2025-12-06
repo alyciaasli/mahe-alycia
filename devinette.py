58096b163475a05147c8f809628faeba5f8c06a4
mahegiogetiluciani-stack <mahegiogetiluciani@gmail.com>
2025-12-06 10:53:03 +0100

    ajouter une devinette

---
+++ devinette.py
@@ -0,0 +1,28 @@
+print("b__bon")
+e = input("Entre le mot complet : ")
+a_e = "bonbon"
+
+if e == a_e:
+    print("correct")
+else:
+    print("faux")
+
+print("b___our")
+a = input("Entre le mot complet : ")
+e_a = "bonjour"
+
+if a == e_a:
+    print("correct")
+else:
+    print("faux")
+
+print("e___ph___")
+
+reponse = input("entre le mots complet : ")
+cacher = "elephant"
+
+if reponse == cacher:
+    print("corecte")
+else:
+    print("faux")
+    print("fin")
