6195d61062825c82b61080cdd0ef4349ededc5c0
alycia asli <alycia.asli@epitech.eu>
2025-12-13 12:14:32 +0100

    ajouter: ajouter des boites

--- mark.py
+++ mark.py
@@ -1,9 +1,20 @@
-print("quelle est la marque de vo chosure")
-marque_chaussure = input()
-if marque_chaussure == "nike":
-    print("accepter")
-else:
-    if marque_chaussure == "jiselle":
-        print("chaussure refuser")
+def type():
+    print("bonjour")
+    a = input()
+    print("oui ")
+
+def marque():
+    print("qu'elle est la marque de vos chaussures")
+    marque_chaussure = input()
+
+    if marque_chaussure == "nike":
+        print("accepter")
+    elif marque_chaussure == "jiselle":
+            print("chaussure refuser")
+    elif marque_chaussure == "adidas":
+            print("chaussure refuser")
     else:
-        print("refuser")
+            print("refuser")
+            type()
+
+marque()
